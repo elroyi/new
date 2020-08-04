b16cd99d5a885ef9e2d7447b720bf1adb1360340
elroyi <elroyi3000@gmail.com>
2020-08-04 10:42:41 +0300

    Updates

---
+++ precache-manifest.c815a7812b1e26140f653e634f083546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a33149122f8914abf160a8b751e233",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "e9866797c5b7ba3d771c",
+    "url": "/new/static/css/main.bcf12e56.chunk.css"
+  },
+  {
+    "revision": "7f8275bbd4bfce1ea3da",
+    "url": "/new/static/js/2.9c281f16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/new/static/js/2.9c281f16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9866797c5b7ba3d771c",
+    "url": "/new/static/js/main.f7dc5228.chunk.js"
+  },
+  {
+    "revision": "f3d469565727a8f5d962",
+    "url": "/new/static/js/runtime-main.709405b7.js"
+  }
+]);
